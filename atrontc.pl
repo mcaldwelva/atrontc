08bd54e3ea8f9f34694ec01581a5b5838aea337d
mcaldwelva <mcaldwelva@gmail.com>
2016-08-02 22:54:40 -0400

    sort to speed-up loading

--- atrontc.pl
+++ atrontc.pl
@@ -1,18 +1,26 @@
 #!/usr/bin/perl
 use File::Find;
-use File::Basename;
 use MP3::Tag;
 use Encode qw( encode decode );
 
-my $base = "/mnt/array1/pub";
+my $share = "/mnt/array1/pub";
 my $folder = "music";
 my $toc = "atrontc.vtc";
 
+%songs = ( );
+
 sub find_music {
     # parse file name
-    my($file, $dir, $ext) = fileparse(decode('utf-8', $_), qr/\.[^.]*/);
+    my $path = decode('utf-8', $_);
+    my($dir, $file, $ext) = ($path =~ m"^(.*/)(.*)(\.mp3|\.m3u|\.wma|\.wav)$");
+    return if $ext eq '';
+
+    # create a new song
     $dir =~ tr/\//\\/;
-    $ext = lc($ext);
+    my $key = $file . $ext . $dir;
+
+    $songs{$key}{DIR} = $dir;
+    $songs{$key}{FILE} = $file . $ext;
 
     # generate MP3::Tag entry
     if ($ext eq '.mp3' ) {
@@ -22,75 +30,66 @@ sub find_music {
         my $tags = $mp3->{ID3v2} if exists $mp3->{ID3v2};
 
         # use file name if no tag
-        my $title = $tags->title() || $file;
+        $songs{$key}{TIT2} = $tags->title() || $file;
 
-        # use No Album if no tag
-        my $album = $tags->album() || 'No Album';
+        # get album name
+        $songs{$key}{TALB} = $tags->album();
 
-        # use No Genre if no tag
-        my $genre = (split(/;/, $tags->genre()))[0] || 'No Genre';
+        # use first genre
+        $songs{$key}{TCON} = (split(/;/, $tags->genre()))[0];
 
         # combine disc with track number to ensure proper order on AT
-        my $track = (split(/\//, $tags->get_frame('TPOS')))[0] . sprintf('%02d', (split(/\//, $tags->track()))[0]) + 0;
+        $songs{$key}{TRCK} = (split(/\//, $tags->get_frame('TPOS')))[0] . sprintf('%02d', (split(/\//, $tags->track()))[0]);
 
         # select the most representative artist for the track
-        my $artist;
-        my $artists = $tags->get_frame('TPE2') . ';' . $tags->get_frame('TPE1') . ';No Artist';
+        my $artists = $tags->get_frame('TPE2') . ';' . $tags->get_frame('TPE1');
         if ($genre eq "Classical") {
             $artists = $tags->get_frame('TCOM') . ';' . $artists
         }
         foreach $item (split(/;/, $artists)) {
 	    if ($item ne '' && $item !~ /^Vario/) {
-		$artist = $item;
+		$songs{$key}{TPE1} = $item;
                 last;
             }
         }
-
-        # add this TOC entry
-        print TOC "SONG\nFILE=$file$ext\nDIR =$dir\nTIT2=$title\nTALB=$album\nTPE1=$artist\nTCON=$genre\nTRCK=$track\nEND \n";
     }
 
     # generate Audio::WMA entry
     if ($ext eq '.wma' ) {
-        my $title = $file;
-        my $album = 'No Album';
-        my $genre = 'No Genre';
-        my $track = 0;
-        my $artist = 'No Artist';
-
-        # add this TOC entry
-        print TOC "SONG\nFILE=$file$ext\nDIR =$dir\nTIT2=$title\nTALB=$album\nTPE1=$artist\nTCON=$genre\nTRCK=$track\nEND \n";
+        $songs{$key}{TIT2} = $file;
     }
 
     # generate Audio::Wav entry
     if ($ext eq '.wav' ) {
-        my $title = $file;
-        my $album = 'No Album';
-        my $genre = 'No Genre';
-        my $track = 0;
-        my $artist = 'No Artist';
-
-        # add this TOC entry
-        print TOC "SONG\nFILE=$file$ext\nDIR =$dir\nTIT2=$title\nTALB=$album\nTPE1=$artist\nTCON=$genre\nTRCK=$track\nEND \n";
+        $songs{$key}{TIT2} = $file;
     }
 
     # generate M3U entry
     if ($ext eq '.m3u' ) {
-        my $title = $file;
-
-        # add this TOC entry
-        print TOC "SONG\nFILE=$file$ext\nDIR =$dir\nTIT2=$title\nEND \n";
+        $songs{$key}{TIT2} = $file;
     }
 }
 
-# look for changed files
-chdir $base;
-if ((! -e $toc) || `find $folder -newer $toc|wc -l` + 0) {
+sub gen_toc {
     open(TOC, '>:encoding(cp1252)', $toc)
       || die "Can't open TOC for writing: $!";
 
-    find({ wanted=>\&find_music, no_chdir=>1 }, $folder);
+    foreach my $key (sort keys %songs) {
+        print TOC "SONG\nFILE=$songs{$key}{FILE}\nDIR =$songs{$key}{DIR}\nTIT2=$songs{$key}{TIT2}\n";
+        print TOC "TALB=$songs{$key}{TALB}\n" if ($songs{$key}{TALB});
+        print TOC "TPE1=$songs{$key}{TPE1}\n" if ($songs{$key}{TPE1});
+        print TOC "TCON=$songs{$key}{TCON}\n" if ($songs{$key}{TCON});
+        print TOC "TRCK=$songs{$key}{TRCK}\n" if ($songs{$key}{TRCK});
+        print TOC "END \n";
+    }
     print TOC "[End TOC]\n\r";
 
     close(TOC);
+}
+
+# look for changed files
+chdir $share;
+if ((! -e $toc) || `find $folder -newer $toc|wc -l` + 0) {
+    find({ wanted=>\&find_music, no_chdir=>1 }, $folder);
+    gen_toc();
 }
